306e97c3b6a693e101b6d26b1893251c1a9484b7
Luccas Oliveira Marinho da Silva <luccas88oliveira@gmail.com>
2025-04-05 00:19:35 -0300

    Construindo componente Todo

--- src/components/Todo/Todo.jsx
+++ src/components/Todo/Todo.jsx
@@ -1,26 +1,84 @@
 import "./Todo.css";
 
-function Todo() {
+import * as React from 'react';
+import TextField from '@mui/material/TextField';
+
+import InputLabel from '@mui/material/InputLabel';
+import FormControl from '@mui/material/FormControl';
+
+
+
+import MenuItem from '@mui/material/MenuItem';
+import Select from '@mui/material/Select';
+
+
+import Button from '@mui/material/Button';
+import Stack from '@mui/material/Stack';
+
+function ContainedButtons() {
     return (
-        <div className="container-todo">
-            <div className="title-area">
-                <h2>To do List</h2>
+        <Stack direction="row" spacing={2}>
+            <Button variant="contained">Contained</Button>
+            <Button variant="contained" disabled>
+                Disabled
+            </Button>
+            <Button variant="contained" href="#contained-buttons">
+                Link
+            </Button>
+        </Stack>
+    );
+}
+
+
+function Todo() {
+    const [priority, setPriority] = React.useState('alta');
+
+    const handleChange = (event) => {
+        setPriority(event.target.value);
+    };
+
+    return (
+        <section className="container-todo">
+            <div className="title-todo">
+                <h3>TODOLIST</h3>
             </div>
-            <div className="task-name-area">
-                <form className="form">
-                    <div className="form-task-name">
-                        <h3>Nome da tarefa</h3>
-                        <input type="text" />
-                    </div>
-                    <div className="form-select-buttonAdd">
-                        <select name="" id="">
-                            <option value="">baixa</option>
-                        </select>
-                        <button>+</button>
-                    </div>
-                </form>
-            </div>
-        </div>
+            <form className="todo-form">
+                <div className="title-input-form">
+                    <p>Digite sua tarefa</p>
+                    <TextField
+                        type="search"
+                        className="input-form"
+                        label="Nome da tarefa"
+                        variant="outlined"
+                        size="small"
+                        placeholder="Tarefa" />
+                </div>
+                <div className="select-form">
+                    <FormControl>
+                        <Select
+                            value={priority}
+                            onChange={handleChange}
+                            size="small"
+                            displayEmpty
+                            inputProps={{ 'aria-label': 'Without label' }}
+                            style={{ width: "80px", height: "40px", fontSize: "12px" }}
+                        >
+                            <MenuItem value="alta">Alta</MenuItem>
+                            <MenuItem value="media">Média</MenuItem>
+                            <MenuItem value="baixa">Baixa</MenuItem>
+                        </Select>
+                    </FormControl>
+                </div>
+                <div className="button-form">
+                    <Button
+                        variant="contained"
+                        style={{ height: "40px", boxShadow: "0 0 0 0" }}>
+                        +
+                    </Button>
+                </div>
+            </form>
+
+        </section>
     )
 }
 
